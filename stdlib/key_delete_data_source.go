269b9985658bf1ac0123bb8a570db57d211cfb95
Matt Schuchard <grapheneowns@gmail.com>
2023-03-22 12:29:12 -0400

    prototype key delete data source

--- stdlib/key_delete_data_source.go
+++ stdlib/key_delete_data_source.go
@@ -7,6 +7,8 @@ import (
   "github.com/hashicorp/terraform-plugin-framework/datasource"
   "github.com/hashicorp/terraform-plugin-framework/datasource/schema"
   "github.com/hashicorp/terraform-plugin-framework/types"
+  "github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+  "github.com/hashicorp/terraform-plugin-framework/diag"
   "github.com/hashicorp/terraform-plugin-log/tflog"
 )
 
@@ -62,5 +64,46 @@ func (tfData *keyDeleteDataSource) Schema(_ context.Context, _ datasource.Schema
 
 // read executes the actual function
 func (tfData *keyDeleteDataSource) Read(ctx context.Context, req datasource.ReadRequest, resp *datasource.ReadResponse) {
+  // determine input values
+  var state keyDeleteDataSourceModel
+  resp.Diagnostics.Append(req.Config.Get(ctx, &state)...)
+  if resp.Diagnostics.HasError() {
+    return
+  }
 
+  deleteKey := state.Key.ValueString()
+  // TODO: allow non-strings with interface or generics
+  var inputMap map[string]string
+  state.Map.ElementsAs(ctx, &inputMap, false)
+
+  // provide debug logging
+  ctx = tflog.SetField(ctx, "stdlib_key_delete_key", deleteKey)
+  ctx = tflog.SetField(ctx, "stdlib_key_delete_map", inputMap)
+  tflog.Debug(ctx, fmt.Sprintf("Input map parameter \"%v\" with key parameter \"%s\" removed", inputMap, deleteKey))
+
+  // delete key from map
+  _, ok := inputMap[deleteKey]; if ok {
+    delete(inputMap, deleteKey)
+  } else {
+    tflog.Error(ctx, fmt.Sprintf("The key to be deleted '%s' does not exist in the input map", deleteKey))
+  }
+
+  // provide more debug logging
+  ctx = tflog.SetField(ctx, "stdlib_key_delete_result", inputMap)
+  tflog.Debug(ctx, fmt.Sprintf("Map with key removed is \"%v\"", inputMap))
+
+  // store resultant map in state
+  state.ID = types.StringValue(deleteKey)
+  // TODO: allow non-strings with interface or generics
+  var mapConvertDiags diag.Diagnostics
+  state.Result, mapConvertDiags = types.MapValueFrom(ctx, basetypes.StringType{}, inputMap)
+  resp.Diagnostics.Append(mapConvertDiags...)
+
+  // set state
+  diagnostics := resp.State.Set(ctx, &state)
+  resp.Diagnostics.Append(diagnostics...)
+  if resp.Diagnostics.HasError() {
+    return
+  }
+  tflog.Info(ctx, "Determined map with key removed", map[string]any{"success": true})
 }
