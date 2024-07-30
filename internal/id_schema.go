0779ca8e2b5ddc76ddae745edf50144f97fc8acf
Matt Schuchard <grapheneowns@gmail.com>
2024-07-30 12:37:19 -0400

    add utility for number type data source id

--- internal/id_schema.go
+++ internal/id_schema.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
 )
@@ -12,6 +13,16 @@ func IDStringAttribute() schema.StringAttribute {
 		Description: "Aliased to string input parameter(s) for efficiency and proper plan diff detection.",
 		PlanModifiers: []planmodifier.String{
 			stringplanmodifier.UseStateForUnknown(),
+		},
+	}
+}
+
+func IDIntAttribute() schema.Int64Attribute {
+	return schema.Int64Attribute{
+		Computed:    true,
+		Description: "Aliased to number input parameter(s) for efficiency and proper plan diff detection.",
+		PlanModifiers: []planmodifier.Int64{
+			int64planmodifier.UseStateForUnknown(),
 		},
 	}
 }
