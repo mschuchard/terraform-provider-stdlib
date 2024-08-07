06c0035526a656201b79c377faee6889a0b2d380
Matt Schuchard <grapheneowns@gmail.com>
2024-08-07 10:32:37 -0400

    add float64 id attribute

--- internal/id_schema.go
+++ internal/id_schema.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/float64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/planmodifier"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringplanmodifier"
@@ -23,6 +24,16 @@ func IDInt64Attribute() schema.Int64Attribute {
 		Description: "Aliased to number input parameter(s) for efficiency and proper plan diff detection.",
 		PlanModifiers: []planmodifier.Int64{
 			int64planmodifier.UseStateForUnknown(),
+		},
+	}
+}
+
+func IDFloat64Attribute() schema.Float64Attribute {
+	return schema.Float64Attribute{
+		Computed:    true,
+		Description: "Aliased to number input parameter(s) for efficiency and proper plan diff detection.",
+		PlanModifiers: []planmodifier.Float64{
+			float64planmodifier.UseStateForUnknown(),
 		},
 	}
 }
