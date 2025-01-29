478aa917a7b0875531c6f909a6f8a4b808674d38
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-01-29 10:50:47 -0500

    add exp function support

---
+++ stdlib/number/exp_test.go
@@ -0,0 +1,58 @@
+package numberfunc_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/function"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+
+	numberfunc "github.com/mschuchard/terraform-provider-stdlib/stdlib/number"
+)
+
+func TestExpFunction(test *testing.T) {
+	test.Parallel()
+
+	standardTestCases := map[string]struct {
+		request  function.RunRequest
+		expected function.RunResponse
+	}{
+		"zero-one": {
+			request: function.RunRequest{
+				Arguments: function.NewArgumentsData([]attr.Value{types.Float64Value(0)}),
+			},
+			expected: function.RunResponse{
+				Result: function.NewResultData(types.Float64Value(1)),
+			},
+		},
+		"float-one-three": {
+			request: function.RunRequest{
+				Arguments: function.NewArgumentsData([]attr.Value{types.Float64Value(1.0986122)}),
+			},
+			expected: function.RunResponse{
+				Result: function.NewResultData(types.Float64Value(2.9999997339956828)),
+			},
+		},
+	}
+
+	for name, testCase := range standardTestCases {
+		test.Run(name, func(test *testing.T) {
+			// initialize result
+			result := function.RunResponse{Result: function.NewResultData(types.Float64Unknown())}
+
+			// execute function and store result
+			numberfunc.NewExpFunction().Run(context.Background(), testCase.request, &result)
+
+			// compare results
+			if !result.Error.Equal(testCase.expected.Error) {
+				test.Errorf("expected value: %s", testCase.expected.Error)
+				test.Errorf("actual value: %s", result.Error)
+			}
+			if !result.Result.Equal(testCase.expected.Result) {
+				test.Errorf("expected value: %+q", testCase.expected.Result.Value())
+				test.Errorf("actual value: %+q", result.Result.Value())
+			}
+		})
+	}
+}
