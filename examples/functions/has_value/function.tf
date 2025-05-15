cd07523951ca0913fecbfc0e2bced1673d12c662
Matthew Schuchard <8029687+mschuchard@users.noreply.github.com>
2025-05-15 15:35:19 -0400

    update deps

--- examples/functions/has_value/function.tf
+++ examples/functions/has_value/function.tf
@@ -1,7 +1,7 @@
 # Check existence of "foo" value in map:
-provider::stdlib::has_key({"hello" = "world", "foo" = "bar"}, "foo")
+provider::stdlib::has_value({"hello" = "world", "foo" = "bar"}, "foo")
 # result => false
 
 # Check existence of "bar" value in map:
-provider::stdlib::has_key({"hello" = "world", "foo" = "bar"}, "bar")
+provider::stdlib::has_value({"hello" = "world", "foo" = "bar"}, "bar")
 # result => true
